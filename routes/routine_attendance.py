d22b36c2f0a4f1ee1f295d51488d0996fee0cabd
Sal Sabila Huq <salsabilahuqnahila@gmail.com>
2026-08-06 00:40:01 +0600

    Update Routine and Attendance UI

--- routes/routine_attendance.py
+++ routes/routine_attendance.py
@@ -3,5 +3,13 @@ from flask import Blueprint, render_template
 routine_bp = Blueprint('routine', __name__)
 
 @routine_bp.route('/routine')
-def routine_page():
-    return render_template('routine_attendance.html')
+def routine_home():
+   
+    students_list = [
+        {"id": "23101411", "name": "Sal Sabila Huq", "status": "Present"},
+        {"id": "23101412", "name": "Mredul Islam", "status": "Present"},
+        {"id": "23101413", "name": "Tanvir Ahmed", "status": "Absent"},
+        {"id": "23101414", "name": "Ayesha Rahman", "status": "Present"},
+        {"id": "23101415", "name": "Naimul Hasan", "status": "Absent"}
+    ]
+    return render_template('routine_attendance.html', students=students_list)
