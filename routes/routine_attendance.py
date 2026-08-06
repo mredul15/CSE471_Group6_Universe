db6e641a72c3b60386c2b709d9d337421156107b
Shah Mohaimin Kabir <shah.mohaimin.kabir@g.bracu.ac.bd>
2026-08-06 13:07:16 +0600

    Delete routes directory

--- routes/routine_attendance.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-routine_bp = Blueprint('routine', __name__)
-
-@routine_bp.route('/routine')
-def routine_page():
-    return render_template('routine_attendance.html')
